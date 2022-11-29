91a4b9c27d6f34374baa3173af1e1b8266d92fc8
Csenger Molnár <molnarcsenger@gmail.com>
2022-11-29 21:18:37 +0100

    Code cleaned

--- src/download_dataset.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-
-import tensorflow_datasets as tfds
-
-# Doesn't work unfortunately, because the source website is down :(
-ds = tfds.load('gtzan', split='train', shuffle_files=True)
